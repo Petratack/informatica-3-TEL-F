26e9b03de1b2daddbb24916cce6e95ef255c405c
Petratack Galantss <patrickgalante25@gmail.com>
2024-05-27 08:36:54 +0000

    prove in classe

---
+++ progetti c++/progetto 27.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout<<"Hello World"<<endl;
+    string name;
+
+    cout<<"Scrivi il tuo nome: ";
+    cin>>name;
+    cout<<endl;
+    cout<<"Ciao "<<name<<endl;
+    return 0;
+
+}
